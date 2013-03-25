f9ecebe49ef9ee33e0c7a33b34423f561dc89675
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-03-24 22:09:01 -0600

    Added skelleton.

---
+++ data/ljredjava.js
@@ -0,0 +1,29 @@
+var app = module.exports = require('appjs');
+
+app.serveFilesFrom(__dirname + '/content');
+
+var window = app.createWindow({
+  width  : 900,
+  height : 600,
+  icons  : __dirname + '/content/icons'
+});
+
+window.on('create', function(){
+  window.frame.show();
+  window.frame.center();
+});
+
+window.on('ready', function(){
+  window.process = process;
+  window.module = module;
+
+  function F12(e){ return e.keyIdentifier === 'F12' }
+  function Command_Option_J(e){ return e.keyCode === 74 && e.metaKey && e.altKey }
+
+  window.addEventListener('keydown', function(e){
+    if (F12(e) || Command_Option_J(e)) {
+      window.frame.openDevTools();
+    }
+  });
+});
+
