f101126dcbc901132bde3e3a486651e335f74e46
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-03-25 01:18:23 -0600

    Updated main page to new device obj names.

--- data/content/js/presenter.js
+++ data/content/js/presenter.js
@@ -1,4 +1,22 @@
+function loadDevice(eventInfo)
+{
+}
+
 function loadDevices()
 {
+    $.getJSON('/api/get_devices.json', function(deviceData)
+    {
+        $.get('/templates/device_list.html', function(template)
+        {
+            $('#device-bar').html(
+                Mustache.render(template, {devices: deviceData})
+            );
 
+            
+        });
+    });
 }
+
+$(document).ready(function() {
+    loadDevices();
+});
